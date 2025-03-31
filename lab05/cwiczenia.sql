f96557c4207ed3e8fc6fe70eb91861d33f4e5338
Joanna <joanna.heldak@gmail.com>
2025-03-31 10:51:53 +0200

    created lab05 files

---
+++ lab05/cwiczenia.sql
@@ -0,0 +1,10 @@
+-- Securing server and databases  
+ 
+
+-- server = server-level security principal (can be mapped to different databases)
+-- user = database-level security principal (can be mapped to one database)
+
+
+
+-- login details are in the master
+-- user details are in the database where they've been created/mapped
